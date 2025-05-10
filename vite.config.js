f96c4dadc1a4dd7cc93541bbf8256c8c440d615c
ysmath0246 <heeju060689@gmail.com>
2025-05-10 22:19:03 +0900

    fix: add Input.jsx with correct casing

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  base: '/admin/', // ✅ 꼭 마지막 '/' 포함!
+export default defineConfig(({ mode }) => ({
+  base: mode === 'development' ? '/' : '/admin/', // ✅ dev에선 '/' / build에선 '/admin/'
   plugins: [react()],
-})
+}))
