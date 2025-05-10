ef90ce7a8c3b3fee0e221e02f086b23c73966ade
ysmath0246 <heeju060689@gmail.com>
2025-05-10 22:06:26 +0900

    fix: Input.jsx case sensitivity

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Link, useLocation } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Link, useLocation } from 'react-router-dom';
 import { AnimatePresence, motion } from 'framer-motion';
 import AttendancePage from './pages/AttendancePage';
 import StudentPage from './pages/StudentPage';
